063e83b8f6725668375b1ecb69e36e48db76573f
Thomas Eggenberger <meshugg83@yahoo.com>
2018-07-19 11:41:06 -0600

    create hello_world

---
+++ hello_world.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" //format
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// You can get a description of a function by typing godoc fmt Println in the terminal
+
+// You execute Go programs like this in the terminal 'go run hello_world.go'
