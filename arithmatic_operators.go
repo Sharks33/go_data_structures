495f692571bbdc3b87bc06d73a92ef96bbf03cae
Thomas Eggenberger <meshugg83@yahoo.com>
2018-07-19 17:58:34 -0600

    arithmatic operators

---
+++ arithmatic_operators.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arithmetic Operators : +, -, *, /, %
+	fmt.Println("6 + 4 =", 6+4)
+	fmt.Println("6 - 4 =", 6-4)
+	fmt.Println("6 * 4 =", 6*4)
+	fmt.Println("6 / 4 =", 6/4)
+	fmt.Println("6 % 4 =", 6%4)
+}
