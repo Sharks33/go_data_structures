7e9a87ecbee6dfc896d03f952037fa351fb1d093
Thomas Eggenberger <meshugg83@yahoo.com>
2018-07-19 18:04:18 -0600

    create logical operators

---
+++ logical_operators.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// AND (&&), OR (||), NOT (!)
+	fmt.Println("True && False = ", true && false)
+	fmt.Println("True || False = ", true || false)
+	fmt.Println("!True = ", !true)
+}
