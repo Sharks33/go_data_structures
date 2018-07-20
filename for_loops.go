d96832ea014c7a11f2d538d3e41eb25c76cb44d9
Thomas Eggenberger <meshugg83@yahoo.com>
2018-07-19 18:14:22 -0600

    create for loops

---
+++ for_loops.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Example 1
+	i := 1
+
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	//Example 2
+	for j := 0; j < 5; j++ {
+		fmt.Println(j)
+	}
+}
